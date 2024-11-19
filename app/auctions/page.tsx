1bc93c1804880b6d95a1e88d08da7f216dd70bf1
cnaples79 <cnaples79@gmail.com>
2024-11-19 18:38:25 -0500

    Update page.tsx

--- app/auctions/page.tsx
+++ app/auctions/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { AuctionGrid } from "@/components/auction-grid";
 
+export const dynamic = "force-dynamic";
+
 export default async function AuctionsPage() {
   const items = await prisma.item.findMany({
     where: { status: "active" },
